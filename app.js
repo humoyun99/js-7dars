11926b79e2fd57abf28ae1855a379521da98c98f
Humoyin <baxranoofxumoyun@gmail.com>
2024-04-01 18:07:50 +0500

    nimadir

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const signupForm=document.querySelector('#signup-form')
 const username=document.querySelector('#userName')
+const message=document.querySelector('.message')
 signupForm.addEventListener('submit',(e)=>{
     e.preventDefault()
-    console.log(username.value)
+    const regEx=/^[a-zA_Z0-9]{6,12}$/
+    const inputVal=signupForm.username.value
 
+    if(regEx.test(inputVal)){
+        message.textContent="malumot tugri kiritildi"
+
+    }else{
+        message.textContent="notugri kiritingiz boshqatanurinib kuring"
+    }
 })
+
