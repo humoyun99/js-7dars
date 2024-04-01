05508ca60f0e3a16e538b53a0ec27ab5a3007517
Humoyin <baxranoofxumoyun@gmail.com>
2024-04-01 18:14:37 +0500

    yanamalumot

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const signupForm=document.querySelector('#signup-form')
-const username=document.querySelector('#userName')
+// const username=document.querySelector('#userName')
 const message=document.querySelector('.message')
 signupForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const regEx=/^[a-zA_Z0-9]{6,12}$/
-    const inputVal=signupForm.username.value
+    const inputVal=signupForm.userName.value
 
     if(regEx.test(inputVal)){
         message.textContent="malumot tugri kiritildi"
